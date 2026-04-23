9f82b93003ddfca4cdd42a61418c31f21bfa228d
Fahsairvw <raveewan.s@ku.th>
2026-04-23 08:21:05 +0700

    edit the file structure

--- domain/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Library, Song
-
-admin.site.register(User)
-admin.site.register(Library)
-admin.site.register(Song)
--- domain/strategies/mock.py
+++
@@ -1,8 +0,0 @@
-class MockSongGenerator:
-
-    def generate(self, data):
-        return {
-            "status": "Succeeded",
-            "url": "https://example.com/mock-song.mp3",
-            "taskId": None
-        }
